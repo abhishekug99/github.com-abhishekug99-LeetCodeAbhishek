23af0f4f187bd03c71a503ba80ca073949c32d0b
abhishekug99 <60284276+abhishekug99@users.noreply.github.com>
2025-02-12 19:00:36 -0500

    Time: 438 ms (38.27%), Space: 33.9 MB (24.38%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -1,17 +1,17 @@
 class Solution:
     def maximumSum(self, nums: List[int]) -> int:
-        sum_map = {}  # Hashmap to store max num for each digit sum
-        max_sum = -1  # Track the max sum of valid pairs
+        sumMap = {}  # Hashmap to store max num for each digit sum
+        maxSum = -1  # Track the max sum of valid pairs
 
         for num in nums:
-            digit_sum = sum(map(int, str(num)))  # Compute sum of digits
+            digitSum = sum(map(int, str(num)))  # Compute sum of digits
             
-            if digit_sum in sum_map:
-                max_sum = max(max_sum, num + sum_map[digit_sum])  # Compare max sum
+            if digitSum in sumMap:
+                maxSum = max(maxSum, num + sumMap[digitSum])  # Compare max sum
             
-            sum_map[digit_sum] = max(sum_map.get(digit_sum, 0), num)  # Store max num for this digit sum
+            sumMap[digitSum] = max(sumMap.get(digitSum, 0), num)  # Store max num for this digit sum
 
-        return max_sum
+        return maxSum
         
         # brutforce 54/83
         # res =[]
