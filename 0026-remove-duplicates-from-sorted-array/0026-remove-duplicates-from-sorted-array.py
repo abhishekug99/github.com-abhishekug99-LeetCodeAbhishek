8cb0004e9b3a29d2e7f1d4b42fb493b45ae0be89
abhishekug99 <60284276+abhishekug99@users.noreply.github.com>
2025-02-20 22:59:15 -0500

    Time: 0 ms (100%), Space: 19 MB (49.77%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        write_index = 1
+        
+        for i in range(1, len(nums)):
+            if nums[i]!=nums[i-1]:
+                nums[write_index] = nums[i]
+                write_index += 1  # Move the write pointer
+        print(nums)
+        return write_index
+
+        
