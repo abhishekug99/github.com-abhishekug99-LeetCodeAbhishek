e45da2e56bc958b4db1ab251b9434f82a9df0921
abhishekug99 <60284276+abhishekug99@users.noreply.github.com>
2025-03-05 00:07:00 -0500

    Time: 2 ms (30.69%), Space: 17.8 MB (42.81%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        
+        stack = []
+        j  = 0
+        if not s:
+            return True
+        if not t:
+            return False
+        for i in range(len(t)):
+            if j < len(s) and t[i] == s[j]:
+                stack.append(t[i])
+                j+=1
+            if ''.join(stack) == s:
+                return True
+                break
+            
+            
+        print(stack)
+
+        
+        return False
+        
