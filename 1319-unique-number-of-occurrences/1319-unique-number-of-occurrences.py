f16666c1bb93b7529f9cc16915e194a2cc1e7951
abhishekug99 <60284276+abhishekug99@users.noreply.github.com>
2025-03-10 22:26:05 -0400

    Time: 3 ms (18.19%), Space: 18.2 MB (20.85%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -1,14 +1,24 @@
 class Solution:
-    def uniqueOccurrences(self, arr: List[int]) -> bool:
-        graph = {}
-        cnt = 1
-        for val in arr:
-            if val in graph:
-                graph[val] += cnt
-            else:
-                graph[val] = cnt
+    # def uniqueOccurrences(self, arr: List[int]) -> bool:
+    #     graph = {}
+    #     cnt = 1
+    #     for val in arr:
+    #         if val in graph:
+    #             graph[val] += cnt
+    #         else:
+    #             graph[val] = cnt
         
-        if len(graph.values()) == len(set(graph.values())):
+    #     if len(graph.values()) == len(set(graph.values())):
+    #         return True
+    #     else:
+    #         return False
+    
+
+
+    #altenate method with counter most efficient
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        cnter = Counter(arr)
+        if len(cnter.values()) == len(set(cnter.values())):
             return True
         else:
             return False
