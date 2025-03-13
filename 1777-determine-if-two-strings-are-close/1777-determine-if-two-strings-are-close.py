a23fc566dc24a5a8fcdeea33268717984642e132
abhishekug99 <60284276+abhishekug99@users.noreply.github.com>
2025-03-13 17:05:35 -0400

    Time: 87 ms (69.26%), Space: 18.3 MB (46.36%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
@@ -1,29 +1,36 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        graphW1 = {}
-        graphW2 = {}
+        #optimised version
+        #using Counter
         if len(word1) != len(word2):
             return False
+        w1Cnt = Counter(word1)
+        w2Cnt = Counter(word2)
         
-        for i in range (len(word1)):
-            if word1[i] not in graphW1:
-                graphW1[word1[i]] = 1
-            else:
-                graphW1[word1[i]] += 1
-        for i in range (len(word2)):
-            if word2[i] not in graphW2:
-                graphW2[word2[i]] = 1
-            else:
-                graphW2[word2[i]] += 1
-        
-        # print(graphW1)
-        # print(graphW2)
-        # print(list(graphW1.values()))
-        # print(list(graphW2.values()))
-        # print(sorted(list(graphW1.keys())))
-        # print(sorted(list(graphW2.keys())))
-        
-        if sorted(graphW1.keys()) == sorted(graphW2.keys()) and sorted(graphW1.values()) == sorted(graphW2.values()):
+        if set(w1Cnt.keys()) == set(w2Cnt.keys()) and sorted(w1Cnt.values()) == sorted(w2Cnt.values()):
             return True
+        
+        return False
 
-        else: return False
+        
+        #beats 5.32% only
+        # graphW1 = {}
+        # graphW2 = {}
+        # if len(word1) != len(word2):
+        #     return False
+        
+        # for i in range (len(word1)):
+        #     if word1[i] not in graphW1:
+        #         graphW1[word1[i]] = 1
+        #     else:
+        #         graphW1[word1[i]] += 1
+        # for i in range (len(word2)):
+        #     if word2[i] not in graphW2:
+        #         graphW2[word2[i]] = 1
+        #     else:
+        #         graphW2[word2[i]] += 1
+        
+        # if sorted(graphW1.keys()) == sorted(graphW2.keys()) and sorted(graphW1.values()) == sorted(graphW2.values()):
+        #     return True
+
+        # else: return False
