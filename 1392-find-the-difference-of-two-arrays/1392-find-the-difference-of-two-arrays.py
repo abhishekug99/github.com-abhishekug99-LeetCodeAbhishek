679a69b5cc89be8ccc422d42565d8f4208bb45a1
abhishekug99 <60284276+abhishekug99@users.noreply.github.com>
2025-03-13 17:25:09 -0400

    Time: 1 ms (97.4%), Space: 18 MB (90.63%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.py
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.py
@@ -1,14 +1,25 @@
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        num1Filter = set()
-        num2Filter = set()
-        res = []
-        for num in nums1:
-            if num not in nums2:
-                num1Filter.add(num)
-        res.append(list(num1Filter))
-        for num in nums2:
-            if num not in nums1:
-                num2Filter.add(num)
-        res.append(list(num2Filter))
-        return res
+        #More Optimised
+        frozenSetNums1 = frozenset(nums1)
+        frozenSetNums2 = frozenset(nums2)
+
+        diff1 = list(frozenSetNums1-frozenSetNums2)
+        diff2 = list(frozenSetNums2-frozenSetNums1)
+        
+        return [diff1,diff2]
+
+        
+        #beats 13.98
+        # num1Filter = set()
+        # num2Filter = set()
+        # res = []
+        # for num in nums1:
+        #     if num not in nums2:
+        #         num1Filter.add(num)
+        # res.append(list(num1Filter))
+        # for num in nums2:
+        #     if num not in nums1:
+        #         num2Filter.add(num)
+        # res.append(list(num2Filter))
+        # return res
