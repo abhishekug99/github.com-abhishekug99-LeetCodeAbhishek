cc151afb53c8bf1d4bcc314daf509f11742066a1
abhishekug99 <60284276+abhishekug99@users.noreply.github.com>
2025-03-18 23:07:24 -0400

    Time: 0 ms (100%), Space: 17.4 MB (99.71%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,33 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums)==1:
+            return nums[0]
+
+        dp = [0]*len(nums)
+        dp[0] = nums[0] 
+        dp[1] = max(nums[0],nums[1])
+
+        for i in range(2,len(nums)):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        
+        return dp[-1]
+        
+        
+        # sum1 = 0
+        # sum2 = 0
+        # if len(nums)<=2:
+        #     return max(nums)
+    
+        # for i in range(len(nums)):
+        #     if i%2==0: 
+        #         sum1 = nums[i] + sum1
+        #     else:
+        #         print(i)
+        #         sum2 = nums[i] + sum2
+        # print(sum1,sum2)
+
+        # return max(sum1, sum2)
+
+        
