a53b67736a6c3cdd34f87b9bd57dc132d775e093
abhishekug99 <60284276+abhishekug99@users.noreply.github.com>
2025-03-24 16:01:31 -0400

    Time: 27 ms (7.28%), Space: 13.8 MB (17.24%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,3 +1,46 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
+class Solution(object):
+    def majorityElement(self, nums):
+        # l = 0
+        # r = 1
+        # count  = 0
+        # maxAppear = {}
+        # maxappearIndex = 0
+        # nums.sort()
+        # if len(nums)<=1:
+        #     return nums[0]
+
+        # for i in range(len(nums)-1):
+        #     if nums[l] == nums[r]:
+        #         maxAppear[nums[i]] = count
+        #         count  = count + 1
+        #     else:
+        #         l=r
+        #     r = r+1
+        # return  max(maxAppear, key= lambda x: maxAppear[x])
+
+        #2
+        if len(nums)<=1:
+            return nums[0]
+    
+        cntAppearance = Counter(nums)
+        return max(cntAppearance, key= lambda x: cntAppearance[x])
+
+        #3
+        # count = {}
+        # result = 0
+        # maxAppear =0
+        # for number in nums:
+        #     count[number] = 1+count.get(number,0)
+        #     result = number if count[number] > maxAppear else result
+        #     maxAppear = max(count[number], maxAppear)
+        # return result
+
+
+            
+
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         
