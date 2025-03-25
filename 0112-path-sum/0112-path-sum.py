a51c3e07ca00a1202358419166a42a96858d8953
abhishekug99 <60284276+abhishekug99@users.noreply.github.com>
2025-03-25 01:25:35 -0400

    Time: 2 ms (33.38%), Space: 19.1 MB (24.12%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -16,14 +16,29 @@ class Solution:
         # return self.hasPathSum(root.left,targetSum) or self.hasPathSum(root.right,targetSum)
 
         #Making to the sum approach DFS apprach:
-        def dfs(root, currSum):
+        # def dfs(root, currSum):
+        #     if not root:
+        #         return False
+        #     currSum += root.val
+        #     if not root.left and not root.right:
+        #         return currSum == targetSum
+        #     return dfs(root.left, currSum) or dfs(root.right, currSum)
+        # return dfs(root,0) 
+
+        #BACKTRACKING
+        path = []
+        def backTrack(root,currSum):
             if not root:
                 return False
+            path.append(root.val)
             currSum += root.val
-            if not root.left and not root.right:
-                return currSum == targetSum
-            return dfs(root.left, currSum) or dfs(root.right, currSum)
-        return dfs(root,0) 
-
-
+            if not root.left and not root.right and currSum == targetSum:
+                return True
+            
+            if backTrack(root.left, currSum) or backTrack(root.right, currSum):
+                return True
+            path.pop()
+            return False
+        
+        return backTrack(root,0)
 
