6e0db9f85526589e9922fb2006bf00daba1c0089
abhishekug99 <60284276+abhishekug99@users.noreply.github.com>
2025-03-27 18:36:40 -0400

    Time: 29 ms (40.33%), Space: 21.8 MB (44.28%) - LeetHub

--- 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.py
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.py
@@ -5,52 +5,52 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    #BFS
-    # def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-    #     if not root:
-    #         return 0
-    #     level = 1
-    #     maxSum = float('-inf')
-    #     maxLevel  = 1
+    #BFS-more optimised
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        level = 1
+        maxSum = float('-inf')
+        maxLevel  = 1
 
-    #     queue = deque([(root,1)])
+        queue = deque([(root,1)])
 
-    #     while queue:
-    #         levelSum = 0
-    #         for _ in range(len(queue)):
-    #             node, lvl = queue.popleft()
-    #             levelSum+=node.val
+        while queue:
+            levelSum = 0
+            for _ in range(len(queue)):
+                node, lvl = queue.popleft()
+                levelSum+=node.val
 
-    #             if node.left:
-    #                 queue.append((node.left,lvl+1))
-    #             if node.right:
-    #                 queue.append((node.right, lvl+1))
-    #         if levelSum > maxSum:
-    #             maxSum = levelSum
-    #             maxLevel = level
-    #         level+=1
-    #     return maxLevel
+                if node.left:
+                    queue.append((node.left,lvl+1))
+                if node.right:
+                    queue.append((node.right, lvl+1))
+            if levelSum > maxSum:
+                maxSum = levelSum
+                maxLevel = level
+            level+=1
+        return maxLevel
 
         
        #DFS
-        def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        # def maxLevelSum(self, root: Optional[TreeNode]) -> int:
  
-            res = {}
+        #     res = {}
             
-            def levels(root, depth):
-                if not root:
-                    return 
-                if depth not in res:
-                    res[depth] = 0
-                res[depth]+=root.val
+        #     def levels(root, depth):
+        #         if not root:
+        #             return 
+        #         if depth not in res:
+        #             res[depth] = 0
+        #         res[depth]+=root.val
 
-                levels(root.left, depth+1)
-                levels(root.right, depth+1)
+        #         levels(root.left, depth+1)
+        #         levels(root.right, depth+1)
                 
-            # curr = 0
+        #     # curr = 0
 
-            levels(root, 1)
-            print(res)
-            return max(res, key=res.get)
+        #     levels(root, 1)
+        #     print(res)
+        #     return max(res, key=res.get)
 
         
