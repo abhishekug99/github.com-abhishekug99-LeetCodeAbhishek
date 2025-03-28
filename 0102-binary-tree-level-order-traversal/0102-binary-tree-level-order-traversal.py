7df92b576634a17fb3c96ee6c4c5ca5b946131ee
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-03-28 01:35:47 -0400

    Time: 0 ms (100%), Space: 18.7 MB (17.97%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -9,23 +9,39 @@ class Solution:
         res = []
         if not root:
             return []
-        queue = deque()
-        print()
-        if root:
-            queue.append(root)
-            print(queue)
-        level = 0
+        queue = deque([(root,1)])
         while len(queue)>0:
             track = []
             for i in range(len(queue)):
-                curr = queue.popleft()
+                curr, lvl = queue.popleft()
                 track.append(curr.val)
                 if curr.left:
-                    queue.append(curr.left)
+                    queue.append((curr.left, lvl+1))
                 if curr.right:
-                    queue.append(curr.right)
+                    queue.append((curr.right, lvl+1))
             res.append(track)
-            level+=1
+     
         return res
+        
+        
+        # res = []
+        # if not root:
+        #     return []
+        # queue = deque()
+        # if root:
+        #     queue.append(root)
+        # level = 0
+        # while len(queue)>0:
+        #     track = []
+        #     for i in range(len(queue)):
+        #         curr = queue.popleft()
+        #         track.append(curr.val)
+        #         if curr.left:
+        #             queue.append(curr.left)
+        #         if curr.right:
+        #             queue.append(curr.right)
+        #     res.append(track)
+        #     level+=1
+        # return res
 
         
