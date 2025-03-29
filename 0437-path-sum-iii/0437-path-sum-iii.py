89fbb1916b917dff66ca0fc744549a21488bac6e
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-03-29 16:26:10 -0400

    Time: 3 ms (88.92%), Space: 18.2 MB (68.54%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -6,24 +6,44 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        res = {}
+        prefixSum = defaultdict(int)
+        prefixSum[0]=1
 
-        def dfs(root, sumTillNow):
-            cnt=0
-            if not root:
+        def dfs(node,sumTillNow):
+            if not node:
                 return 0
-            sumTillNow += root.val
-            if  sumTillNow == targetSum:
-                cnt+=1
-            # if  sumTillNow > targetSum:
-            #     sumTillNow-=root.val
-            
-            cnt += dfs(root.left, sumTillNow)
-            cnt += dfs(root.right, sumTillNow)
+            sumTillNow +=node.val
+            cnt = prefixSum[sumTillNow - targetSum]
+            prefixSum[sumTillNow] += 1
+
+            cnt+=dfs(node.left,sumTillNow)
+            cnt+=dfs(node.right,sumTillNow)
+
+            prefixSum[sumTillNow] -= 1
             return cnt
-        if not root:
-            return 0
-        return dfs(root, 0) + self.pathSum(root.left, targetSum) + self.pathSum(root.right, targetSum) 
+
+        return dfs(root,0)
+
+
+        #works O(nlagn) and worst case O(n^2)
+        # def dfs(root, sumTillNow):
+        #     cnt=0
+        #     if not root:
+        #         return 0
+        #     sumTillNow += root.val
+        #     if  sumTillNow == targetSum:
+        #         cnt+=1
+        #     # if  sumTillNow > targetSum:
+        #     #     sumTillNow-=root.val
+            
+        #     cnt += dfs(root.left, sumTillNow)
+        #     cnt += dfs(root.right, sumTillNow)
+        #     return cnt
+
+        # if not root:
+        #     return 0
+    
+        # return dfs(root, 0) + self.pathSum(root.left, targetSum) + self.pathSum(root.right, targetSum)
         
 
         
