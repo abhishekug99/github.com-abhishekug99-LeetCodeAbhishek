ea0b0bff5341106affb575bea89098b41d9893a9
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-04-03 21:53:58 -0400

    Time: 91 ms (23.96%), Space: 33.5 MB (28.6%) - LeetHub

--- 1474-longest-zigzag-path-in-a-binary-tree/1474-longest-zigzag-path-in-a-binary-tree.py
+++ 1474-longest-zigzag-path-in-a-binary-tree/1474-longest-zigzag-path-in-a-binary-tree.py
@@ -6,23 +6,43 @@
 #         self.right = right
 class Solution:
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
-        maxLen = 0
-        def dfs(root, isRight, lenghtCnt):
-            nonlocal maxLen
+        # similar but more optimised one
+        self.logestOne = 0
+        def dfs(root,pathDir,depth):
             if not root:
                 return 0
-            
-            maxLen = max(maxLen, lenghtCnt)
+            self.logestOne = max(self.logestOne, depth)
 
-            if isRight:
-                dfs(root.left, False, lenghtCnt+1)
-                dfs(root.right,True,1)
-            else:
-                dfs(root.right, True, lenghtCnt+1)
-                dfs(root.left,False,1)
-            return maxLen        
+            if pathDir == 'right':
+                dfs(root.right, 'left', depth+1)
+                dfs(root.left, 'right', 1)
+            if pathDir == 'left':
+                dfs(root.left, 'right', depth+1)
+                dfs(root.right, 'left', 1)
         
-        dfs(root.left, False, 1)
-        dfs(root.right, True, 1)
-        return maxLen
+        dfs(root.right,'left',1)
+        dfs(root.left,'right',1)
+        return self.logestOne
+
+        
+        #works fine for all test cases
+        # maxLen = 0
+        # def dfs(root, isRight, lenghtCnt):
+        #     nonlocal maxLen
+        #     if not root:
+        #         return 0
+            
+        #     maxLen = max(maxLen, lenghtCnt)
+
+        #     if isRight:
+        #         dfs(root.left, False, lenghtCnt+1)
+        #         dfs(root.right,True,1)
+        #     else:
+        #         dfs(root.right, True, lenghtCnt+1)
+        #         dfs(root.left,False,1)
+        #     return maxLen        
+        
+        # dfs(root.left, False, 1)
+        # dfs(root.right, True, 1)
+        # return maxLen
         
