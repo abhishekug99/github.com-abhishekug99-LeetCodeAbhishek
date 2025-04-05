7e4371c574dfd3e73c861b96c420fa7ae3cd6ca4
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-04-05 15:49:21 -0400

    Time: 39 ms (99.04%), Space: 22.2 MB (21.8%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,52 +7,47 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        pathP = []
-        pathQ = []
+        if not root:
+            return 
+        if root == q or root == p:
+            return root
+        nodeL = self.lowestCommonAncestor(root.left,p,q)
+        nodeR = self.lowestCommonAncestor(root.right,p,q)
+        if nodeL and nodeR:
+            return root
+        if  not nodeL and not nodeR:
+            return
+        if nodeL:
+            return nodeL
+        else: 
+            return nodeR
 
-        def findPath(node, target, path):
-            if not node:
-                return False
-            path.append(node)
-            if node == target:
-                return True
-            if findPath(node.left, target, path) or findPath(node.right, target, path):
-                return True
-            path.pop()
-            return False
-
-        findPath(root, p, pathP)
-        findPath(root, q, pathQ)
-        # print(pathP)
-        # print(pathQ)
-        
-        i=0
-        while i < len(pathP) and i < len(pathQ) and pathP[i] == pathQ[i]:
-            i += 1
-        return pathQ[i-1] if i>0 else None
 
         
-        # ance = []
-        # # dece = []
-        # def Ance(root):
-        #     if not root:
+        # space O(1)
+        # pathP = []
+        # pathQ = []
+
+        # def findPath(node, target, path):
+        #     if not node:
+        #         return False
+        #     path.append(node)
+        #     if node == target:
         #         return True
-        #     if root.val == p.val:
+        #     if findPath(node.left, target, path) or findPath(node.right, target, path):
         #         return True
-        #     if Ance(root.left) or Ance(root.right):
-        #         ance.append(root.val)
-        #         return True
+        #     path.pop()
         #     return False
-        # def Dece(root):
-        #     if not root:
-        #         return []
-        #     dece = [root.val]
-        #     dece.extend(Dece(root.left))
-        #     dece.extend(Dece(root.right))
-        #     return dece
+
+        # findPath(root, p, pathP)
+        # findPath(root, q, pathQ)
+        # # print(pathP)
+        # # print(pathQ)
         
-        # Ance(root)
-        # dece=Dece(p)
+        # i=0
+        # while i < len(pathP) and i < len(pathQ) and pathP[i] == pathQ[i]:
+        #     i += 1
+        # return pathQ[i-1] if i>0 else None
         
 
 
