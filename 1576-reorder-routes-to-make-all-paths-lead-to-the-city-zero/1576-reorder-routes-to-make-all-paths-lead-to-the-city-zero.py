712eb1b18b3c28d3f2138f52a6b1ad143b1cb9ac
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-04-08 21:11:02 -0400

    Time: 248 ms (19.06%), Space: 53.5 MB (17.72%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,45 +1,51 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        graph = defaultdict(list)
-        for a, b in connections:
-            graph[a].append((b,1))
-            graph[b].append((a,0))
-        # print(graph)
+        #start with city 0
+        # recursively check nbrs
+        # count out goinf edges
+
+        edges = {(a,b) for a,b in connections}
+        nbrs = { city:[] for city in range(n)}
+        visit = set() #keep track of visit
+        changes =0
+        for a,b in connections: # to fill nbrs hash map
+            nbrs[a].append(b)
+            nbrs[b].append(a)
+        def dfs(city ):
+            nonlocal edges,nbrs,visit, changes
+            for nbr in nbrs[city]:
+                if nbr in visit:
+                    continue
+                #check if nbr reaches the city
+                if (nbr,city) not in edges:
+                    changes+=1
+                visit.add(nbr)
+                dfs(nbr)
         
-        visit = set()
-        def dfs(node):
-            visit.add(node)
-            cnt = 0
-            for nbr, needsReverse in graph[node]:
-                if nbr not in visit:
-                    cnt+= needsReverse #handle tuples with 0
-                    cnt+=dfs(nbr)
-            return cnt
-        return dfs(0)
-        
+        visit.add(0)
+        dfs(0)
+        return changes
+
+
+
+
+        # graph = defaultdict(list)
+        # for a, b in connections:
+        #     graph[a].append((b,1))
+        #     graph[b].append((a,0))
+        # # print(graph)
         
         # visit = set()
-        # leadToZero = int()
-        # def dfs(r, paths):
+        # def dfs(node):
+        #     visit.add(node)
         #     cnt = 0
-        #     leadToZero = int()
-        #     if 0 in paths[r] and paths[r][1]==0:
-        #         leadToZero = r
-        #     if 0 in paths[r] and paths[r][0]==0:
-        #         leadToZero = paths[r][1]
-        #         cnt+=1
-        #     if paths[r] and paths[r][1] == leadToZero:
-        #         leadToZero = paths[r][0]
-        #     else:
-        #         leadToZero = paths[r][1]
-            
+        #     for nbr, needsReverse in graph[node]:
+        #         if nbr not in visit:
+        #             cnt+= needsReverse
+        #             cnt+=dfs(nbr)
         #     return cnt
-
-        # final = 0
-        # for r in range(len(connections)):
-        #     final+=dfs(r, connections)
-
-        # return final
+        # return dfs(0)
+    
 
 
             
