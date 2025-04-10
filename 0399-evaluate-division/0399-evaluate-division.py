44d2fadc23932d269791d13e8a404f7e455c02c6
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-04-09 20:38:43 -0400

    Time: 0 ms (100%), Space: 17.8 MB (74.9%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,33 +1,75 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        graph = defaultdict(list)
-        for (a,b), val in zip(equations, values):
-            graph[a].append((b,val))
-            graph[b].append((a,1/val))
-        # print(graph)
+        # time O(e+v)
+        # space O(v+e)
+        #DFS
+        # graph = defaultdict(list) #map a->list of [b,a/b]
+        # for (a,b), val in zip(equations, values):
+        #     graph[a].append((b,val)) #straight given weights
+        #     graph[b].append((a,1/val)) #reverse weights, inverses of equations
+        # # print(graph)
 
-        def dfs(curr, target, visited, productTrack):
-            if curr == target:
-                return productTrack
-            visited.add(curr)
-            for nbr, val in graph[curr]:
-                if nbr not in visited:
-                    res = dfs(nbr, target,visited, productTrack*val)
-                    if res!=-1:
-                        return res
-            return -1    
-        res = []
-        for a,b in queries:
-            if a not in graph or b not in graph:
-                res.append(-1.0)
-            elif a==b:
-                res.append(1)
-            else:
-                visited = set()
-                res.append(dfs(a,b,visited,1.0))
-        return res
+        # def dfs(curr, target, visited, productTrack):
+        #     if curr == target:
+        #         return productTrack
+        #     visited.add(curr)
+        #     for nbr, val in graph[curr]:
+        #         if nbr not in visited:
+        #             res = dfs(nbr, target,visited, productTrack*val)
+        #             if res!=-1:
+        #                 return res
+        #     return -1    
+        # res = []
+        # for a,b in queries:
+        #     if a not in graph or b not in graph:
+        #         res.append(-1.0)
+        #     elif a==b:
+        #         res.append(1)
+        #     else:
+        #         visited = set()
+        #         res.append(dfs(a,b,visited,1.0))
+        # return res
+
+        #implementaion with BFS
+        adj = defaultdict(list)
+        for i, eq in enumerate(equations):
+            a,b = eq
+            adj[a].append((b,values[i]))
+            adj[b].append((a,1/values[i]))
+        
+        def bfs(src, target):
+            if src not in adj or target not in adj:
+                return -1
+            queue = deque()
+            visit = set()
+            queue.append([src, 1]) #to go layer by layer, so to keep track of simulatniously multiplication
+            visit.add(src)
+            while queue:
+                node,weight= queue.popleft()
+                if node == target:
+                    return weight
+
+                for nbr, weightInAdj in adj[node]:
+                    if nbr not in visit: #if not visited traverse the bfs
+                        queue.append([nbr,weight * weightInAdj])
+                        visit.add(nbr)
+
+            return -1
+        
+        return [bfs(q[0],q[1])for q in queries]
 
 
+
+
+
+
+
+
+
+
+
+
+        #does not work but basic logic for understanding
         # res = []
         # visit = set()
         # varSet = set()
