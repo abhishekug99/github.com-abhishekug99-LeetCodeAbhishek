b4640282a9c2cc0c3646a064f4e36097751f0e3e
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-04-11 16:21:40 -0400

    Time: 6 ms (53.87%), Space: 18.6 MB (59.92%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,15 +1,19 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        # print(bin(n)[2:])
-        res=[]
-        
-        for i in range(n+1):
-            count=0
-            bi=i
-            # bi = int(bin(i)[2:])
-            while bi>0:
-                bi &= (bi-1)
-                count+=1
-            res.append(count)
+        #dp
+        res = [0] * (n + 1)
+        for i in range(1, n + 1):
+            res[i] = res[i >> 1] + (i & 1)
+        return res
+
+        # res=[]
+        # for i in range(n+1):
+        #     count=0
+        #     bi=i
+        #     # bi = int(bin(i)[2:])
+        #     while bi>0:
+        #         bi &= (bi-1)
+        #         count+=1
+        #     res.append(count)
     
-        return(res)
+        # return(res)
