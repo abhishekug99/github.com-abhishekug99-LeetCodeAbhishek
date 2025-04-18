a66f2da86f24f327e81469d99503052a3aeb49b9
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-04-18 14:57:48 -0400

    Time: 3 ms (98.35%), Space: 17.9 MB (64.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,28 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        res=[]
-        sList = list(s)
+        #Optimal
         if len(s)!=len(t):
             return False
-        for char in t:
-            if char in sList:
-                sList.remove(char)
-            else:
-               return False
+        cntS = Counter(s)
+        cntT = Counter(t)
+        # print(cntS)
+        for k, v in cntS.items():
+            if k not in cntT.keys():
+                return False
+            if v!=cntT[k]:
+                return False
         return True
+
+
+        #BruteForce O(n)
+        # res=[]
+        # sList = list(s)
+        # if len(s)!=len(t):
+        #     return False
+        # for char in t:
+        #     if char in sList:
+        #         sList.remove(char)
+        #     else:
+        #        return False
+        # return True
         # return False    
