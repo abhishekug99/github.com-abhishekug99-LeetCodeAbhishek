a8536f5a6f1464136882e5b56c255a35bc00ca95
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-04-22 16:52:22 -0400

    Time: 0 ms (100%), Space: 18.1 MB (19.42%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -9,13 +9,13 @@ class Solution:
                 return 0
         l,r = 0,len(nums)-1
         maxNum = max(nums)
-        while l<=r:
-            mid =(l+r)//2
-            if maxNum<nums[mid]:
-                l=mid+1
-            elif maxNum>nums[mid]:
-                r = mid-1
-            else:
-                return mid
+        # while l<=r:
+        #     mid =(l+r)//2
+        #     if maxNum<nums[mid]:
+        #         l=mid+1
+        #     elif maxNum>nums[mid]:
+        #         r = mid-1
+        #     else:
+        #         return mid
         return nums.index(maxNum)
             
