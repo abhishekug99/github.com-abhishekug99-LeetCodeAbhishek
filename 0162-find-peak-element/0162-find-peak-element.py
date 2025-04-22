589ea3c16c0a0b4ffde381302e8a414a852b0299
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-04-22 16:51:06 -0400

    Time: 0 ms (100%), Space: 17.9 MB (62.46%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return 0
+        if len(nums)==2:
+            if nums[1]>nums[0]:
+                return 1
+            else:
+                return 0
+        l,r = 0,len(nums)-1
+        maxNum = max(nums)
+        while l<=r:
+            mid =(l+r)//2
+            if maxNum<nums[mid]:
+                l=mid+1
+            elif maxNum>nums[mid]:
+                r = mid-1
+            else:
+                return mid
+        return nums.index(maxNum)
+            
