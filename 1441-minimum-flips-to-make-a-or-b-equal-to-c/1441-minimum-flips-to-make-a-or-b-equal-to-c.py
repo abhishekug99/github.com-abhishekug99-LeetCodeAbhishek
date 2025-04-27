edacc4966d6fa95812bb4e69755ff04d1ea0dcf6
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-04-27 15:08:36 -0400

    Time: 30 ms (94.13%), Space: 17.7 MB (49.84%) - LeetHub

---
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        counter = 0
+        while a>0 or b>0 or c>0:
+            aBi = a & 1
+            bBi = b & 1
+            cBi = c & 1
+            if (aBi|bBi)!=cBi:
+                if cBi ==0:
+                    counter+=(aBi+bBi)
+                else:
+                    counter+=1
+            a>>=1
+            b>>=1
+            c>>=1
+        return counter
+        
+        # counter = 0
+        # orth = a|b
+        # if orth == c:
+        #     return 0
+        # # while orth!=c:
+        # for _ in range(max(a,b)):
+        #     if orth<c:
+        #         orth=orth|1
+        #         counter+=1
+        #     if orth >c:
+        #         orth= ~orth+1
+        #         counter-=1
+        
+        # return counter
+
+        
