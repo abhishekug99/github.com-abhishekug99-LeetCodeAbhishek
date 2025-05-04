a96290799819671f107a9981bdaa26a1de109637
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-05-03 20:04:46 -0400

    Time: 0 ms (100%), Space: 17.7 MB (65.16%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,28 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # Actual bottom to up dp O(m)
+        prevRow = [0]*n
+        for r in range(m-1,-1,-1):
+            currRow = [0]*n
+            currRow[n-1] = 1
+            for c in range(n-2,-1,-1):
+                currRow[c] = currRow[c+1]+prevRow[c]
+            prevRow = currRow
+        return prevRow[0]
+
+        
+        # top to bottom dp O(m*n)
+        # cache = [[0]*n for _ in range(m)]
+        # # print(cache)
+        # def memoization(r,c,row,col):
+        #     if r == row or c == col:
+        #         return 0
+        #     if cache[r][c]>0:
+        #         return cache[r][c]
+        #     if r == row-1 or c == col-1:
+        #         return 1
+        #     cache[r][c] = (memoization(r+1,c,row,col) + memoization(r,c+1,row,col))
+        #     return cache[r][c]
+    
+        # return memoization(0, 0, m, n)
+        
