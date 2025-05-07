ab19036a6b11042b92a4a37ee143182bc5dc67b1
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-05-07 18:36:05 -0400

    Time: 400 ms (22.15%), Space: 23.6 MB (41.85%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,36 +1,39 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-    
-        # cash = 0
-        # hold = -prices[0]
+        # dp-most optimal
+        cash = 0
+        hold = -prices[0]
 
-        # for price in prices[1:]:
-        #     cash = max(cash, hold + price - fee)
-        #     hold = max(hold, cash - price)
+        for price in prices[1:]:
+            cash = max(cash, hold + price - fee)
+            print(cash)
+            hold = max(hold, cash - price)
+            print(hold)
+        return cash
+        
+        #dfs
+        # dp = {} #key (i,buy), val = maxprofit
+        # def dfs(i, buying):
 
-        # return cash
-        dp = {} #key (i,buy), val = maxprofit
-        def dfs(i, buying):
+        #     if i==len(prices):
+        #         return 0
 
-            if i==len(prices):
-                return 0
+        #     if (i, buying) in dp:
+        #         return dp[(i, buying)]
 
-            if (i, buying) in dp:
-                return dp[(i, buying)]
-
-            if buying:
-                buy = dfs(i+1, False) - prices[i]
-                skip  = dfs(i+1,True)
-                dp[(i, buying)] = max(buy, skip)
+        #     if buying:
+        #         buy = dfs(i+1, False) - prices[i]
+        #         skip  = dfs(i+1,True)
+        #         dp[(i, buying)] = max(buy, skip)
             
-            else:
-                sell = dfs(i+1, True) + prices[i] - fee
-                hold = dfs(i+1, False)
-                dp[(i, buying)] = max(sell,hold)
+        #     else:
+        #         sell = dfs(i+1, True) + prices[i] - fee
+        #         hold = dfs(i+1, False)
+        #         dp[(i, buying)] = max(sell,hold)
             
-            return dp[(i, buying)]
+        #     return dp[(i, buying)]
         
 
-        return dfs(0,True)
+        # return dfs(0,True)
             
           
