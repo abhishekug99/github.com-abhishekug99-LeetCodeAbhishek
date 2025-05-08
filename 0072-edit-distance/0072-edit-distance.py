e022ee846f993c6a5d28b04be105c6a12eca0d7f
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-05-08 18:08:10 -0400

    Time: 63 ms (64.32%), Space: 21.3 MB (34.02%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -6,7 +6,7 @@ class Solution:
             cache[len(word1)][j] = len(word2)-j
         for i in range(len(word1)+1):
             cache[i][len(word2)] = len(word1)-i
-        print(cache)
+        # print(cache)
         for i in range(len(word1)-1,-1,-1):
             for j in range(len(word2)-1,-1,-1):
                 if word1[i] == word2[j]:
