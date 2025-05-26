deb1b74aa6b259bef10b287d0455fee35ee8f5c2
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-05-26 15:50:16 -0400

    Time: 5 ms (65.74%), Space: 18.3 MB (9.58%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,16 +1,16 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        return [s.index(ch) for ch in s] ==  [t.index(ch) for ch in t]
-        # def transform(string):
-        #     iMap = {}
-        #     result = []
-        #     for i, ch in enumerate(string):
-        #         if ch not in iMap:
-        #             iMap[ch] = i
-        #         result.append(iMap[ch])
-        #     return result
-
-        # return transform(s) == transform(t)
+        # High time
+        # return [s.index(ch) for ch in s] ==  [t.index(ch) for ch in t]
+        def transform(string):
+            iMap = {}
+            result = []
+            for i, ch in enumerate(string):
+                if ch not in iMap:
+                    iMap[ch] = i
+                result.append(iMap[ch])
+            return result
+        return transform(s) == transform(t)
         
         
         
