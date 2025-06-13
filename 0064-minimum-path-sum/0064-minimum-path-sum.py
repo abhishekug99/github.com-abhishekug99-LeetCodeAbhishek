011ead16aabf4fac7dddbe36eee264bcdab55213
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-06-13 18:48:24 -0400

    Time: 11 ms (90.39%), Space: 20.4 MB (32.07%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,22 +1,40 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
+        #dp O(m*n)
         ROW = len(grid)
         COL = len(grid[0])
-        cache = {}
-        def dfs(r,c):
-            if r >= ROW or c >= COL:
-                return float('inf')
-            if  r==ROW-1 and c ==COL-1:
-                return grid[r][c]
-            if (r,c) in cache:
-                return cache[(r,c)]
-            
-            right = dfs(r,c+1)
-            down = dfs(r+1,c)
-            cache[(r,c)] = grid[r][c] + min(right,down)
-            print(cache)
-            return cache[(r,c)]
+        dp = [[float('inf')]*(COL+1) for i in range(ROW+1)]
+        dp[ROW][COL-1] = 0
+        # print(dp)
+        for r in range(ROW-1,-1,-1):
+            for c in range(COL-1,-1,-1):
+                dp[r][c] = grid[r][c]+min(dp[r+1][c],dp[r][c+1])
+                # print(dp)
+        
 
-        return dfs(0,0)
+        return dp[0][0]
+
+        
+        
+
+        #dfs
+        # ROW = len(grid)
+        # COL = len(grid[0])
+        # cache = {}
+        # def dfs(r,c):
+        #     if r >= ROW or c >= COL:
+        #         return float('inf')
+        #     if  r==ROW-1 and c ==COL-1:
+        #         return grid[r][c]
+        #     if (r,c) in cache:
+        #         return cache[(r,c)]
+            
+        #     right = dfs(r,c+1)
+        #     down = dfs(r+1,c)
+        #     cache[(r,c)] = grid[r][c] + min(right,down)
+        #     # print(cache)
+        #     return cache[(r,c)]
+
+        # return dfs(0,0)
          
     
