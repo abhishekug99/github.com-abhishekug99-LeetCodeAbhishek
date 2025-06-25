1facfdfef7c892f41b30029c2df83977f68104f3
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-06-25 17:06:49 -0400

    Time: 87 ms (33.56%), Space: 89.5 MB (13.45%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -6,30 +6,45 @@
 #         self.right = right
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-        if not inorder or not postorder:
+        
+        if not postorder or not inorder:
             return None
-        treeMap = {}
-        for i, val in enumerate(inorder):
-            treeMap[val] = i
-        postIdx = [len(postorder) - 1]
+        rootVal = postorder[-1]
+        root = TreeNode(rootVal)
 
-        def insert(l,r):
-            if l>r:
-                return None
+        i = inorder.index(rootVal)
 
-            rootVal = postorder[postIdx[0]]
-            postIdx[0] -= 1
+        root.left = self.buildTree(inorder[:i], postorder[:i])
+        root.right = self.buildTree(inorder[i+1:], postorder[i:-1])
 
-            root = TreeNode(rootVal)
-            rootIdx = treeMap[rootVal]
+        return root
+        
+        
+        #O(n) due to map
+        # if not inorder or not postorder:
+        #     return None
+        # treeMap = {}
+        # for i, val in enumerate(inorder):
+        #     treeMap[val] = i
+        # postIdx = [len(postorder) - 1]
 
-            root.right = insert(rootIdx+1, r)
-            root.left = insert(l, rootIdx-1)
+        # def insert(l,r):
+        #     if l>r:
+        #         return None
+
+        #     rootVal = postorder[postIdx[0]]
+        #     postIdx[0] -= 1
+
+        #     root = TreeNode(rootVal)
+        #     rootIdx = treeMap[rootVal]
+
+        #     root.right = insert(rootIdx+1, r)
+        #     root.left = insert(l, rootIdx-1)
             
 
-            return root
+        #     return root
         
         
-        return insert(0,len(inorder)-1)
+        # return insert(0,len(inorder)-1)
 
         
