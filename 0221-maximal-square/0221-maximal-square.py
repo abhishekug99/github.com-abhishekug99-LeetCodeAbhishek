4cd33e49ded4b3f3dc56b65804a3b9aa4b66f290
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-07-11 14:40:07 -0400

    Time: 131 ms (61.96%), Space: 33.2 MB (40.79%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,22 +1,44 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        dp = []
-        ROW = len(matrix)+1
-        COL = len(matrix[0])+1
-
-        for i in range(len(matrix)+1):
-            dp.append([0]*COL)
+        # Saving more timme
+        if not matrix or not matrix[0]:
+            return 0
+    
+        ROW = len(matrix)
+        COL = len(matrix[0])
         
-        for r in range(ROW-1):
-            for c in range(COL-1):
-                if matrix[r][c] == '1':
-                    dp[r][c] = 1+min(dp[r-1][c], dp[r][c-1], dp[r-1][c-1])
-                    
-                elif matrix[r][c] == '0':
-                    dp[r][c] = 0
+        dp = []
+        for _ in range(ROW + 1):
+            dp.append([0] * (COL + 1))
 
         maxSqr = 0
-        for rows in dp:
-            maxSqr = max(maxSqr, max(rows))
+
+        for r in range(1, ROW + 1):
+            for c in range(1, COL + 1):
+                if matrix[r - 1][c - 1] == '1':
+                    dp[r][c] = 1 + min(dp[r - 1][c], dp[r][c - 1], dp[r - 1][c - 1])
+                    maxSqr = max(maxSqr, dp[r][c])
         
-        return maxSqr**2 
+        return maxSqr ** 2
+
+        #my logic -Accepted O(R*C)
+        # dp = []
+        # ROW = len(matrix)+1
+        # COL = len(matrix[0])+1 #extra row and col for not goin out of bound
+
+        # for i in range(len(matrix)+1):
+        #     dp.append([0]*COL)
+        
+        # for r in range(ROW-1):
+        #     for c in range(COL-1):
+        #         if matrix[r][c] == '1':
+        #             dp[r][c] = 1+min(dp[r-1][c], dp[r][c-1], dp[r-1][c-1]) # main logic
+                    
+        #         elif matrix[r][c] == '0':
+        #             dp[r][c] = 0
+
+        # maxSqr = 0
+        # for rows in dp:
+        #     maxSqr = max(maxSqr, max(rows))
+        
+        # return maxSqr**2 
