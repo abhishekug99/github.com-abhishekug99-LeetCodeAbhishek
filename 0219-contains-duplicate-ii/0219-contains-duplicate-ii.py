d10e449a2c3dd337bb1a503dfb7b352b6c8562e0
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-07-12 12:24:24 -0400

    Time: 36 ms (32.69%), Space: 37.1 MB (12.12%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,18 +1,15 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        n = len(nums)
         graph = {}
-
         for i, val in enumerate(nums):
-            if val in graph.keys():
-                if abs(i - graph[val]) <=k:
-                    return True
+            if val in graph.keys() and abs(i - graph[val]) <=k:
+                return True
             graph[val] = i
                     
         return False    
 
 
-
+        # n = len(nums)
         # while i<n:
         #     for j in range(i+1, n):
         #         if nums[i] == nums[j] and abs(i-j)<=k:
