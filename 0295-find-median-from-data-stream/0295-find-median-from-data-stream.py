d0f4ced0c0cc7053ef502f3c1fa1e670da655bac
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-08-09 17:07:06 -0400

    Time: 171 ms (34.64%), Space: 40.3 MB (9.93%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,22 +1,47 @@
 import heapq
 class MedianFinder:
+#nlog(n) as bisect.insort uses binary search apperoch to find insertion point
+    # def __init__(self):
+    #     self.arr = []
+    #     # heapq.heapify(self.arr)
 
+    # def addNum(self, num: int) -> None:
+    #     bisect.insort(self.arr, num)   
+
+    # def findMedian(self) -> float:
+    #     median = 0
+    #     if len(self.arr)%2 == 0:
+    #         idx = (len(self.arr)-1)//2
+    #         median = (self.arr[idx] + self.arr[idx+1])/2
+    #     elif len(self.arr)%2 != 0:
+    #         idx =(len(self.arr))//2
+    #         median = self.arr[idx]
+    #     return median
+ 
+# Heap based min max both heaps
     def __init__(self):
-        self.arr = []
+        self.small = [] #maxHeap
+        self.large = [] #minHeap
         # heapq.heapify(self.arr)
 
     def addNum(self, num: int) -> None:
-        bisect.insort(self.arr, num)   
+        heapq.heappush(self.small, -num) #add max heap
+
+        if self.small and self.large and (-self.small[0]>self.large[0]):
+            heapq.heappush(self.large, -heapq.heappop(self.small))
+
+        #balance size
+        if len(self.small)>len(self.large) +1:
+            heapq.heappush(self.large, -heapq.heappop(self.small))
+        if len(self.large)>len(self.small):
+            heapq.heappush(self.small, -heapq.heappop(self.large))
+
+
 
     def findMedian(self) -> float:
-        median = 0
-        if len(self.arr)%2 == 0:
-            idx = (len(self.arr)-1)//2
-            median = (self.arr[idx] + self.arr[idx+1])/2
-        elif len(self.arr)%2 != 0:
-            idx =(len(self.arr))//2
-            median = self.arr[idx]
-        return median
+        if len(self.small) > len(self.large):
+            return -self.small[0]
+        return (-self.small[0] + self.large[0]) / 2
     
 
 # Your MedianFinder object will be instantiated and called as such:
