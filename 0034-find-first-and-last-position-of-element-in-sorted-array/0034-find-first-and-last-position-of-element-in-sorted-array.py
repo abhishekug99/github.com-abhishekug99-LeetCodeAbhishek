2ef89e983949ca077be5b4ebad88a1ee572de385
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-08-18 12:03:12 -0400

    Time: 0 ms (100%), Space: 19.1 MB (20.62%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,37 +1,37 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         #O(logN)
-        # if target not in nums:
-        #     return[-1,-1]
+        if target not in nums:
+            return[-1,-1]
         
-        # l,r = 0, len(nums)-1
-        # i=-1
-        # while l<=r:
-        #     mid = (l+r)//2
-        #     if nums[mid] == target:
-        #         i=mid
-        #         break
-        #     elif target > nums[mid]:
-        #         l = mid+1
-        #     else:
-        #         r = mid-1
-        # if i==-1:
-        #     return[-1,-1]
-        # left ,right= i,i
-        # while left>0 and nums[left-1]==target:
-        #     left -=1
-        # while right<len(nums)-1 and nums[right+1] == target:
-        #     right+=1
+        l,r = 0, len(nums)-1
+        i=-1
+        while l<=r:
+            mid = (l+r)//2
+            if nums[mid] == target:
+                i=mid
+                break
+            elif target > nums[mid]:
+                l = mid+1
+            else:
+                r = mid-1
+        if i==-1:
+            return[-1,-1]
+        left ,right= i,i
+        while left>0 and nums[left-1]==target:
+            left -=1
+        while right<len(nums)-1 and nums[right+1] == target:
+            right+=1
         
-        # return [left,right]
+        return [left,right]
 
-        #using bisect
-        import bisect
+        #using bisect #O(logN)
+        # import bisect
 
-        left = bisect.bisect_left(nums,target)
-        right = bisect.bisect_right(nums,target)
+        # left = bisect.bisect_left(nums,target)
+        # right = bisect.bisect_right(nums,target)
 
-        if left < right and left<len(nums) and nums[left]==target and nums[right-1]==target:
-            return [left,right-1]
-        return [-1,-1]
+        # if left < right and left<len(nums) and nums[left]==target and nums[right-1]==target:
+        #     return [left,right-1]
+        # return [-1,-1]
 
