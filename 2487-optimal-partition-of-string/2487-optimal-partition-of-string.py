90ff7fce4e14d216b46202230bac3e903523dfde
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-08-19 12:54:29 -0400

    Time: 47 ms (79.1%), Space: 17.9 MB (98.84%) - LeetHub

--- 2487-optimal-partition-of-string/2487-optimal-partition-of-string.py
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.py
@@ -1,18 +1,29 @@
 class Solution:
     def partitionString(self, s: str) -> int:
-        i=0
+        #optimised to O(n) and extra space
+        visited = set()
+        cnt =1
+        for ch in s:
+            if ch in visited:
+                cnt+=1
+                visited.clear()
+            visited.add(ch)
+        return cnt
+                
         
-        subSList = []
-        while i< len(s):
-            subS = s[i]
-            j=i+1
-            while j<len(s):
-                if s[j] not in  subS:
-                    subS += s[j]
-                else:
-                    break
-                j+=1
-            subSList.append(subS)
-            i=j   
-        print(subSList)
-        return len(subSList)
+        #Accepted ans works in O(n^2) and extra space
+        # i=0 
+        # subSList = []
+        # while i< len(s):
+        #     subS = s[i]
+        #     j=i+1
+        #     while j<len(s):
+        #         if s[j] not in  subS:
+        #             subS += s[j]
+        #         else:
+        #             break
+        #         j+=1
+        #     subSList.append(subS)
+        #     i=j   
+        # print(subSList)
+        # return len(subSList)
