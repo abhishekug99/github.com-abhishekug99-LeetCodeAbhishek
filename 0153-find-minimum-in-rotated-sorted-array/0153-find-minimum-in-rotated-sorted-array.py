ab2d9b666b10a20b97365cc2f9e8ccd016197a38
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-08-22 13:38:45 -0400

    Time: 2 ms (4.25%), Space: 18 MB (80.13%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -4,17 +4,17 @@ class Solution:
         #return min(nums) ->O(n)
 
         #most optimal O(logn)
-        # l,r = 0, len(nums)-1
-        # while l<r:
-        #     mid = (l+r)//2
-        #     if nums[mid] > nums[r]:
-        #         l = mid+1
-        #     else:
-        #         r = mid
-        # return nums[l]
+        l,r = 0, len(nums)-1
+        while l<r:
+            mid = (l+r)//2
+            if nums[mid] > nums[r]:
+                l = mid+1
+            else:
+                r = mid
+        return nums[l]
         
         # bruteforce
-        finalMin = nums[0]
-        for num in nums:
-            finalMin = min(num, finalMin)
-        return finalMin
+        # finalMin = nums[0]
+        # for num in nums:
+        #     finalMin = min(num, finalMin)
+        # return finalMin
