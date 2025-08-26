82009edb3712a4913c3c80bbd2b0d41bb1cfd742
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-08-26 11:04:29 -0400

    Time: 5 ms (38.29%), Space: 19.1 MB (88.38%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,12 +1,20 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        cntMap = {}
-        for i in range(len(nums)):
-            if nums[i] not in cntMap:
-                cntMap[nums[i]] =1
-            else:
-                cntMap[nums[i]] += 1
-        for k,v in cntMap.items():
-            if v ==1:
-                return k
-                break
+        #bit manupulation with XOR
+        ones, twos = 0, 0
+        for num in nums:
+            ones = (ones ^ num) & ~twos
+            twos = (twos ^ num) & ~ones
+        return ones
+
+        #normal
+        # cntMap = {}
+        # for i in range(len(nums)):
+        #     if nums[i] not in cntMap:
+        #         cntMap[nums[i]] =1
+        #     else:
+        #         cntMap[nums[i]] += 1
+        # for k,v in cntMap.items():
+        #     if v ==1:
+        #         return k
+        #         break
