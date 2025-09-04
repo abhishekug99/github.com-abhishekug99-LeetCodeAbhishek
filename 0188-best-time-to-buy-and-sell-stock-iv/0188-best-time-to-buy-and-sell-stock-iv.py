5210bfe6c033b692b64eb61170e892192f618a09
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-09-04 14:17:58 -0400

    Time: 54 ms (71.25%), Space: 18.7 MB (62.24%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -4,12 +4,12 @@ class Solution:
         if n<=1:
             return 0
         
-        # if k>=n//2: # if k is very large , its same as unlimited transactions
-        #     profit=0
-        #     for i in range(1, n):
-        #         if prices[i]>prices[i-1]:
-        #             profit+= prices[i] - prices[i-1]
-        #     return profit
+        if k>=n//2: # if k is very large , its same as unlimited transactions
+            profit=0
+            for i in range(1, n):
+                if prices[i]>prices[i-1]:
+                    profit+= prices[i] - prices[i-1]
+            return profit
 
         dp = [[0] * n for _ in range(k+1)]
         for t in range(1,k+1):
