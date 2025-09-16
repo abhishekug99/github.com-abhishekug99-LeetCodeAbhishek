4a44ebe69c0cf2b6cc1737a4659dbc894c6f1414
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-09-16 14:23:05 -0400

    Time: 297 ms (14.13%), Space: 31.7 MB (36.96%) - LeetHub

---
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
+        def getGCD(a,b):
+            while b:
+                a,b = b, a%b
+            return a
+        def getLCM(x,y):
+            return x * y // getGCD(x, y)
+
+        print(getLCM(6,4))
+        res = []
+        for num in nums:
+            res.append(num)
+            while len(res)>1:
+                g = getGCD(res[-1],res[-2])
+                if g==1:
+                    break
+                lcm = getLCM(res[-1],res[-2])
+                res.pop()
+                res[-1]=lcm
+            
+        return res
+        
