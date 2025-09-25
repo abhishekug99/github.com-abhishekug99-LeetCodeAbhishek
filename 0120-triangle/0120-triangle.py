ba042ee1d6d3fe1f379c7749cd74288ed74f3c60
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-09-25 13:46:55 -0400

    Time: 3 ms (86%), Space: 19.2 MB (39.88%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,20 +1,27 @@
+import heapq
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        for row in range(len(triangle) - 2, -1, -1):
-            for col in range(len(triangle[row])):
-                triangle[row][col] += min(triangle[row + 1][col], triangle[row + 1][col + 1])
+        if len(triangle) == 1:
+            return triangle[0][0]
         
+        for i in range(len(triangle)-2,-1,-1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(triangle[i+1][j],triangle[i+1][j+1] )
         return triangle[0][0]
+
+        # res = triangle[0][0]
+        # idx  = 0
+        # minVal=0
+        # for i in range(1, len(triangle)): 
+        #     for j in range(len(triangle[i])):
+        #         if i == 1:
+        #             minVal = min(triangle[i])
+        #             res+=minVal
+        #             idx = triangle[i].index(minVal)
+        #         else:
+        #             minVal = min(triangle[i][idx], triangle[i][idx+1])
+        #             res+=minVal
+        #             idx = triangle[i].index(minVal)
+        # return res
+
         
-        # if len(triangle) == 1:
-        #     return min(triangle[0])
-        # sumMin = 0
-        # j=0
-        # for i in range(len(triangle)):
-        #     if i == 0:
-        #         sumMin+=min(triangle[j])
-        #     else:
-        #         sumMin+=min(triangle[j:])
-        #         j+=1
-        
-        # return sumMin
