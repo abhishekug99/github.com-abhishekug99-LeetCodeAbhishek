460dad1bab834ae5261318b11f77ad37d2d0d500
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-09-25 13:52:01 -0400

    Time: 4 ms (54.43%), Space: 18.8 MB (74.79%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,27 +1,19 @@
 import heapq
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        if len(triangle) == 1:
-            return triangle[0][0]
+        dp = [0]*(len(triangle)+1)
+        for r in triangle[::-1]:
+            for i,n in enumerate(r):
+                dp[i] = min(dp[i], dp[i+1])+n
         
-        for i in range(len(triangle)-2,-1,-1):
-            for j in range(len(triangle[i])):
-                triangle[i][j] += min(triangle[i+1][j],triangle[i+1][j+1] )
-        return triangle[0][0]
+        return dp[0]
 
-        # res = triangle[0][0]
-        # idx  = 0
-        # minVal=0
-        # for i in range(1, len(triangle)): 
+        # if len(triangle) == 1:
+        #     return triangle[0][0]
+        
+        # for i in range(len(triangle)-2,-1,-1):
         #     for j in range(len(triangle[i])):
-        #         if i == 1:
-        #             minVal = min(triangle[i])
-        #             res+=minVal
-        #             idx = triangle[i].index(minVal)
-        #         else:
-        #             minVal = min(triangle[i][idx], triangle[i][idx+1])
-        #             res+=minVal
-        #             idx = triangle[i].index(minVal)
-        # return res
+        #         triangle[i][j] += min(triangle[i+1][j],triangle[i+1][j+1] )
+        # return triangle[0][0]
 
         
