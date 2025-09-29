bab79a0ecf61582b438b9676cff665b9d7abc095
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-09-29 10:25:17 -0400

    Time: 39 ms (61.11%), Space: 17.8 MB (84.72%) - LeetHub

---
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minScoreTriangulation(self, values: List[int]) -> int:
+        n = len(values)
+        dp = [[0] * n for _ in range(n)]
+
+        for idx in range(3, n+1):
+            for i in range(0, n-idx+1):
+                j = i+idx-1
+                dp[i][j] = float('inf')
+                for k in range(i+1, j):
+                    cost = dp[i][k] + dp[k][j] + values[i]*values[j]*values[k]
+                    dp[i][j] = min(dp[i][j], cost)
+        print(dp)
+        return dp[0][n-1]
