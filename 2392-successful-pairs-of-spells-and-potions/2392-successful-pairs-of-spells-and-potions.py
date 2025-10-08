f3df03a849b1bbcf443f44496ec0850c6461d0a1
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-10-08 12:12:14 -0400

    Time: 887 ms (8.88%), Space: 42.6 MB (43.37%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.py
@@ -16,34 +16,4 @@ class Solution:
                     ans = mid
                     r = mid-1
             res.append(len(potions)-ans)
-            # x = len(potions[ans:])
-            # if x:
-            #     res.append(x)
-            # else:
-            #     res.append(0)
         return res
-        
-        # works but take so much time for sorting at every iteration, not good for too big data , test case 49
-        # for i in range(len(spells)):
-        #     # cnt = 0
-        #     potionsSpells = sorted(list(map(lambda x:x*spells[i], potions))) #takes too much time sorts every iteration
-        #     l,r = 0, len(potionsSpells)-1
-        #     ans = len(potionsSpells)
-        #     while l<=r:
-        #         mid  = (l+r)//2
-        #         if success>potionsSpells[mid]:
-        #             l = mid+1
-        #         elif success<=potionsSpells[mid]:
-        #             ans = mid
-        #             r=mid-1
-                    
-        #     x = len(potionsSpells[ans:])
-        #     if x:
-        #         res.append(x)
-        #     else: 
-            
-        #         res.append(0)
-
-        # return res
-
-        # print(potionsSpells)
