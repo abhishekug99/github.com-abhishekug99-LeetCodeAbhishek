73667acd99a5b51f253dfe744ee55e64006c06b5
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-10-10 22:56:11 -0400

    Time: 460 ms (71%), Space: 46.3 MB (42.5%) - LeetHub

---
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.py
@@ -0,0 +1,45 @@
+class Solution:
+    def maximumTotalDamage(self, power: List[int]) -> int:
+        cnt = Counter(power)
+        damage = {x: x*freq for x, freq in cnt.items()}
+        unique = sorted(damage.keys())
+
+        n= len(unique)
+        dp = [0]*n
+        dp[0] = damage[unique[0]]
+
+        for i in range(1,n):
+            take = damage[unique[i]]
+            j = i-1
+            while j>=0 and unique[i]-unique[j]<=2:
+                j-=1
+            if j>=0:
+                take+=dp[j]
+            dp[i] = max(dp[i-1],take)
+        
+        return dp[-1]
+
+
+        
+        
+        #works for 330/554 test cases
+        # dp = []
+        # for p in power:
+        #     tmp=[p-1,p-2,p+1,p+2]
+        #     dp.append(tmp)
+
+        # cast = 0
+        # for i in range(len(power)):
+        #     # npv = [power[i]-2, power[i]-1, power[i]+1, power[i]+2]
+        #     curCast = power[i]
+        #     for j in range(i+1, len(power)):
+        #         if  power[j] not in dp[i]:
+        #             curCast += power[j]
+        #         else:
+        #             continue
+        #     print(curCast)
+        #     cast = max(curCast, cast)
+        #     print(cast)
+        
+        # return cast
+            
