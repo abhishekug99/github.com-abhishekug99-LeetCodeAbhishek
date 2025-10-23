34840af77014aa1c27183cb53eefd706ee473940
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-10-23 12:27:24 -0400

    Time: 87 ms (30.16%), Space: 17.7 MB (93.88%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.py
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.py
@@ -3,7 +3,6 @@ class Solution:
         i=0
         n = len(s)
 
-        
         while len(s)>2:
             tmp=''
             for j in range(len(s)-1):
@@ -11,5 +10,4 @@ class Solution:
                 tmp+= str((int(s[j])+ int(s[j+1]))% 10) 
             s = tmp
             
-        print(s)
         return len(s) == 2 and s[0]==s[1]
