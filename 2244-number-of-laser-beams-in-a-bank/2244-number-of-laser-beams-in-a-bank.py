e0832a723a39f59f1569411a7ecb6c41ddde2e8c
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-10-27 11:21:38 -0400

    Time: 4 ms (83.82%), Space: 19.5 MB (44.64%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.py
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.py
@@ -1,17 +1,30 @@
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
+        #O(n)
+        curr = bank[0].count('1')
         res = 0
-        n = len(bank)
 
-        for i in range(n-1):
-            if '1' in bank[i]:
-                cnti = Counter(bank[i])
-                for j in range(i+1, n):
-                    if '1' not in bank[j]:
-                        continue
-                    else:
-                        cntj = Counter(bank[j])
-                        # print(cntj)
-                        res += cntj['1']*cnti['1']
-                        break
+        for row in bank[1:]:
+            numDev = row.count('1')
+            if not numDev:
+                continue
+            res += numDev*curr
+            curr =  numDev
         return res
+ 
+        #accepted O(n*m)
+        # res = 0
+        # n = len(bank)
+
+        # for i in range(n-1):
+        #     if '1' in bank[i]:
+        #         cnti = Counter(bank[i])
+        #         for j in range(i+1, n):
+        #             if '1' not in bank[j]:
+        #                 continue
+        #             else:
+        #                 cntj = Counter(bank[j])
+        #                 # print(cntj)
+        #                 res += cntj['1']*cnti['1']
+        #                 break
+        # return res
