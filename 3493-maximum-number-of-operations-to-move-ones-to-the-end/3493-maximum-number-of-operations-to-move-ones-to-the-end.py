18afaad543b536bab4f833aa48965f4b478a3154
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-11-13 10:19:50 -0500

    Time: 75 ms (38.32%), Space: 18 MB (94.61%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.py
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.py
@@ -1,15 +1,29 @@
 class Solution:
     def maxOperations(self, s: str) -> int:
-        res = 0
-        ones = 0
-        i = 0
-        while i<len(s):
-            if s[i] =='1':
-                ones+=1
+        #reverse order way
+        zeros = 0
+        res=0
+
+        for i in reversed(range(len(s))):
+            if s[i] == '0':
+                if (i == len(s)-1 or s[i+1] =='1'):
+                    zeros+=1
             else:
-                while i+1<len(s) and s[i+1] == '0':
-                    i+=1
-                res+=ones    
-            i+=1
+                res+=zeros
         return res
+            
+        
+        #works O(n)
+        # res = 0
+        # ones = 0
+        # i = 0
+        # while i<len(s):
+        #     if s[i] =='1':
+        #         ones+=1
+        #     else:
+        #         while i+1<len(s) and s[i+1] == '0':
+        #             i+=1
+        #         res+=ones    
+        #     i+=1
+        # return res
                 
