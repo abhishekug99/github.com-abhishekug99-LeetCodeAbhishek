44e6d51377a2f75f1b8eda0bc6116999101fd630
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-11-14 18:42:32 -0500

    Time: 3 ms (8.7%), Space: 17.9 MB (32.43%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,20 +1,35 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        j = len(needle)
-        if haystack == needle:
-            return 0
-        if len(needle)==1 and needle in haystack:
-            return haystack.index(needle)
-        if len(needle)>len(haystack):
+        if not needle or len(needle)>len(haystack):
             return -1
-        for i in range(len(haystack)-len(needle)+1):
-            print(haystack[i:j])
-            if haystack[i:j] == needle and j<=len(haystack):
-                return i
-            j+=1
-            
+        if needle==haystack :
+            return 0
+
+        lps = [0]*len(needle)
+        prev = 0
+        i=1
+        while i < len(needle):
+            if needle[i] == needle[prev]:
+                prev+=1
+                lps[i] = prev
+                i+=1
+            else:
+                if prev!=0:
+                    prev = lps[prev-1]
+                else:
+                    lps[i]=0
+                    i+=1
+        i=j=0
+        while i<len(haystack):
+            if haystack[i] == needle[j]:
+                i+=1
+                j+=1
+            else:
+                if j!=0:
+                    j = lps[j-1] #fallback
+                else:
+                    i+=1
+            if j == len(needle):
+                return i-j
         return -1
-
-
-
-        
+                
