aeee251192ddd3c262bd20a496bc86f375ae516a
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-11-18 16:47:07 -0500

    Time: 3 ms (51.92%), Space: 18.3 MB (18.84%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,15 +1,49 @@
 import bisect
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        for num in nums2:
-            idx = bisect.bisect_left(nums1, num)
-            nums1.insert(idx, num)
-        print(nums1[-1]+nums1[0])
+        A, B = nums1, nums2
+        total = len(nums1)+len(nums2)
+        half = total//2
+
+        if len(B)<len(A):
+            A,B = B,A
         
-        if len(nums1) %2 != 0:
-            return nums1[len(nums1)//2]
+        l,r = 0, len(A) -1
+
+        while True:
+            i = (l+r)//2 #A
+            j = half - i - 2 #B
+            Aleft = A[i] if i>=0 else float('-inf')
+            Aright = A[i+1] if (i+1)<len(A)  else float('inf')
+            Bleft = B[j] if j>=0 else float('-inf')
+            Bright = B[j+1] if (j+1)<len(B)  else float('inf')
+
+            if Aleft<=Bright and Bleft<=Aright:
+                #odd
+                if total%2:
+                    return min(Aright, Bright)
+                #even  
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            
+            elif Aleft > Bright:
+                r = i-1
+            else:
+                l = i+1
         
-        res  = (nums1[len(nums1)//2] +  nums1[(len(nums1)//2)-1])/2
-        return res
+
+
+
+        
+        
+        # for num in nums2:
+        #     idx = bisect.bisect_left(nums1, num)
+        #     nums1.insert(idx, num)
+        # print(nums1[-1]+nums1[0])
+        
+        # if len(nums1) %2 != 0:
+        #     return nums1[len(nums1)//2]
+        
+        # res  = (nums1[len(nums1)//2] +  nums1[(len(nums1)//2)-1])/2
+        # return res
 
         
