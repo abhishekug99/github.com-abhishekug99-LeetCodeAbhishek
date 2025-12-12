c533c72cdd0dd812eac261116e4b69bf1797b9c6
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-12-12 12:17:42 -0500

    Time: 38 ms (92.08%), Space: 17.9 MB (95.09%) - LeetHub

---
+++ 3433-count-mentions-per-user/3433-count-mentions-per-user.py
@@ -0,0 +1,71 @@
+class Solution:
+    def countMentions(self, numberOfUsers: int, events: List[List[str]]) -> List[int]:
+        online = [True]*numberOfUsers
+        backOnline = [-1]*numberOfUsers
+        mensions = [0]*numberOfUsers #res
+        # returnAt = defaultdict(list)
+        pq = [] #minheap
+        events.sort(key=lambda x: (int(x[1]), 0 if x[0] == "OFFLINE" else 1))
+        allCnt =0
+        for event in events:
+            typ, ts, info = event
+            ts = int(ts)
+            
+            while pq and pq[0][0]<=ts:
+                retTime, u = heapq.heappop(pq)
+                if backOnline[u] ==retTime:
+                    online[u] = True
+            if typ == 'OFFLINE':
+                u = int(info)
+                online[u]=False
+                retTime=ts+60
+                backOnline[u] = retTime
+                heapq.heappush(pq, (retTime, u))
+            else:
+                if info=="ALL":
+                    allCnt+=1
+                elif info=="HERE":
+                    for u in range(numberOfUsers):
+                        if online[u]:
+                            mensions[u]+=1
+                else:
+                    for t in info.split():
+                        u=int(t[2:])
+                        mensions[u]+=1
+        if allCnt:
+            for u in range(numberOfUsers):
+                mensions[u]+=allCnt
+                
+        return mensions
+
+
+        # for event in events:
+        #     typ, ts, info = event
+        #     ts = int(ts)
+            
+        #     if ts in returnAt:
+        #         for user in returnAt[ts]:    
+        #             online[user] = True
+        #         del returnAt[ts]
+                
+        #     if typ == 'OFFLINE':
+        #         user = int(info) #as offline its will be int str
+        #         online[user] = False
+        #         returnAt[ts+60].append(user)
+
+        #     else: #if online the third would some message
+        #         mensionStr = info
+        #         if mensionStr == 'ALL':
+        #             for i in range(numberOfUsers):
+        #                 mensions[i]+=1
+        #         elif mensionStr == 'HERE':
+        #             for i in range(numberOfUsers):
+        #                 if online[i]:
+        #                     mensions[i]+=1
+        #         else:
+        #             tocken = mensionStr.split()
+        #             for t in tocken:
+        #                 user = int(t[2:]) #id in form 'id0' 
+        #                 mensions[user]+=1
+        
+        # return mensions
