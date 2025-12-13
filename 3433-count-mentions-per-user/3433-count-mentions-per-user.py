aa64ffdb92e27b94f6bb3e3a981a1cf4adc85ff0
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-12-12 19:01:03 -0500

    Time: 31 ms (98.48%), Space: 17.8 MB (95.06%) - LeetHub

--- 3433-count-mentions-per-user/3433-count-mentions-per-user.py
+++ 3433-count-mentions-per-user/3433-count-mentions-per-user.py
@@ -3,42 +3,67 @@ class Solution:
         online = [True]*numberOfUsers
         backOnline = [-1]*numberOfUsers
         mensions = [0]*numberOfUsers #res
-        # returnAt = defaultdict(list)
-        pq = [] #minheap
-        events.sort(key=lambda x: (int(x[1]), 0 if x[0] == "OFFLINE" else 1))
-        allCnt =0
+        allCnt = 0
+        events.sort(key=lambda x: (int(x[1]), x[0] == "MESSAGE"))
+
         for event in events:
-            typ, ts, info = event
-            ts = int(ts)
-            
-            while pq and pq[0][0]<=ts:
-                retTime, u = heapq.heappop(pq)
-                if backOnline[u] ==retTime:
-                    online[u] = True
-            if typ == 'OFFLINE':
-                u = int(info)
-                online[u]=False
-                retTime=ts+60
-                backOnline[u] = retTime
-                heapq.heappush(pq, (retTime, u))
-            else:
-                if info=="ALL":
+            if "MESSAGE" == event[0]:
+                if "ALL" == event[2]:
                     allCnt+=1
-                elif info=="HERE":
-                    for u in range(numberOfUsers):
-                        if online[u]:
-                            mensions[u]+=1
+                elif "HERE" == event[2]:
+                    time = int(event[1])
+                    for i in range(numberOfUsers):
+                        if online[i]<=time:
+                            mensions[i]+=1
                 else:
-                    for t in info.split():
-                        u=int(t[2:])
-                        mensions[u]+=1
-        if allCnt:
-            for u in range(numberOfUsers):
-                mensions[u]+=allCnt
-                
+                    for sid in event[2].split():
+                        mensions[int(sid[2:])]+=1
+            else:
+                online[int(event[2])] = int(event[1])+60
+        
+        for i in range(numberOfUsers):
+            mensions[i]+=allCnt
         return mensions
 
 
+        
+        # Heap solution O(nlogn)
+        # pq = [] #minheap
+        # events.sort(key=lambda x: (int(x[1]), 0 if x[0] == "OFFLINE" else 1))
+        # allCnt =0
+        # for event in events:
+        #     typ, ts, info = event
+        #     ts = int(ts)
+            
+        #     while pq and pq[0][0]<=ts:
+        #         retTime, u = heapq.heappop(pq)
+        #         if backOnline[u] ==retTime:
+        #             online[u] = True
+        #     if typ == 'OFFLINE':
+        #         u = int(info)
+        #         online[u]=False
+        #         retTime=ts+60
+        #         backOnline[u] = retTime
+        #         heapq.heappush(pq, (retTime, u))
+        #     else:
+        #         if info=="ALL":
+        #             allCnt+=1
+        #         elif info=="HERE":
+        #             for u in range(numberOfUsers):
+        #                 if online[u]:
+        #                     mensions[u]+=1
+        #         else:
+        #             for t in info.split():
+        #                 u=int(t[2:])
+        #                 mensions[u]+=1
+        # if allCnt:
+        #     for u in range(numberOfUsers):
+        #         mensions[u]+=allCnt
+                
+        # return mensions
+
+        # good but not accepted, bugs
+        # returnAt = defaultdict(list)
         # for event in events:
         #     typ, ts, info = event
         #     ts = int(ts)
