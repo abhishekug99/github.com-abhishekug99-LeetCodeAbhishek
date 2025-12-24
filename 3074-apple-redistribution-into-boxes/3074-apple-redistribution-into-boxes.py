7aacf8b54e8213043f0660e7eb1cb7a04076319e
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-12-24 11:04:04 -0500

    Time: 0 ms (100%), Space: 17.2 MB (97.51%) - LeetHub

--- 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.py
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.py
@@ -1,21 +1,37 @@
 class Solution:
     def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
-        n = len(apple)
-        m = len(capacity)
-        totalApples = sum(apple)
-        res = 1
-        if totalApples in capacity:
-            return 1
-        if totalApples >= sum(capacity):
-            return len(capacity)
-        
+
+        total = sum(apple)
+        if total == 0:
+            return 0
+
         capacity.sort(reverse=True)
-        for i in range(m):
-            if capacity[i]<totalApples:
-                res+=1
-                totalApples-=capacity[i]
-            elif capacity[i]>=totalApples:
-                return res
-        return res
+
+        running = 0
+        for i, c in enumerate(capacity):
+            running += c
+            if running >= total:
+                return i + 1
+
+        return -1
+
+        
+        # n = len(apple)
+        # m = len(capacity)
+        # totalApples = sum(apple)
+        # res = 1
+        # if totalApples in capacity:
+        #     return 1
+        # if totalApples >= sum(capacity):
+        #     return len(capacity)
+        
+        # capacity.sort(reverse=True)
+        # for i in range(m):
+        #     if capacity[i]<totalApples:
+        #         res+=1
+        #         totalApples-=capacity[i]
+        #     elif capacity[i]>=totalApples:
+        #         return res
+        # return res
             
         
