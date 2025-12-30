64c23c9c03da16cbde0d1311c000450f19edd354
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2025-12-30 10:45:07 -0500

    Time: 459 ms (52.02%), Space: 17.9 MB (87.37%) - LeetHub

--- 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.py
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.py
@@ -1,23 +1,54 @@
 class Solution:
     def pyramidTransition(self, bottom: str, allowed: List[str]) -> bool:
+        # with memoisation
         substrMap  = defaultdict(list)
         for pattern in allowed:
             substrMap[(pattern[0],pattern[1])].append(pattern[2])
-        @lru_cache
-        def dfs(curr, i, above)->bool:
+        
+        memo = {}
+
+        def dfs(curr, i, above):
             if len(curr)==1:
                 return True
-            if i == len(curr)-1:
-                return dfs(above, 0, "")
             
+            if i ==0 and curr in memo:
+                return memo[curr]
+            
+            if i==len(curr)-1:
+                res = dfs(above,0,'')
+                memo[curr] = res
+                return res
             pair = (curr[i], curr[i+1])
-            if pair not in  substrMap:
+            if pair not in substrMap:
                 return False
             
             for char in substrMap[pair]:
-                if dfs(curr, i+1, above + char):
+                if dfs(curr, i+1, above+char):
                     return True
-                # above = above[:-1]
             return False
-        
+            
         return dfs(bottom,0,"" )
+
+
+        #Submitted works well
+        # substrMap  = defaultdict(list)
+        # for pattern in allowed:
+        #     substrMap[(pattern[0],pattern[1])].append(pattern[2])
+        
+        # def dfs(curr, i, above)->bool:
+        #     if len(curr)==1:
+        #         return True
+        #     if i == len(curr)-1:
+        #         return dfs(above, 0, "")
+            
+        #     pair = (curr[i], curr[i+1])
+        #     if pair not in  substrMap:
+        #         return False
+            
+        #     for char in substrMap[pair]:
+        #         if dfs(curr, i+1, above + char):
+        #             return True
+        #         # above = above[:-1]
+        #     return False
+        
+        # return dfs(bottom,0,"" )
