53bf6241fc9be50a473a98e7e56c267fd3d74eb1
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2026-02-22 20:09:17 -0500

    Time: 3 ms (98.97%), Space: 20.4 MB (25.21%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,8 +2,7 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         if not s:
             return True
-        s = s.lower()
-        s = re.sub(r'[, ;:.]', "", s)
+        s = ''.join(c for c in s if c.isalnum()).lower()
         print(s)
         if s==s[::-1]:
             return True
