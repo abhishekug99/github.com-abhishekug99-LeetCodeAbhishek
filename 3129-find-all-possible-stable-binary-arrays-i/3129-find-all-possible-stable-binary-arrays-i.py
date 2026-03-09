4903a2c4f0d33a1291ca4e1e6cbec185aa790c8a
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2026-03-09 11:42:06 -0400

    Time: 12525 ms (6.06%), Space: 60.8 MB (34.85%) - LeetHub

---
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
+        MOD = 10**9+7
+        t = [[[-1 for _ in range(2)] for _ in range(201)] for _ in range(201)]
+        def solve(onesLeft, zerosLeft, lastWasOne, limit):
+            if onesLeft == 0 and zerosLeft == 0:
+                return 1
+            if t[onesLeft][zerosLeft][lastWasOne] != -1:
+                return t[onesLeft][zerosLeft][lastWasOne]
+            
+            res=0 
+            if lastWasOne:
+                l=1
+                for l in range(1, min(limit, zerosLeft) + 1):
+                    res = (res + solve(onesLeft, zerosLeft - l, 0, limit)) % MOD
+            else:
+                l=1
+                for l in range(1, min(limit, onesLeft) + 1):
+                    res = (res + solve(onesLeft - l, zerosLeft, 1, limit)) % MOD
+            t[onesLeft][zerosLeft][lastWasOne] = res
+            return res
+        
+        startwithone = solve(one, zero, 0, limit)
+        startwithzero = solve(one, zero, 1, limit)
+
+        return (startwithone + startwithzero) % MOD
