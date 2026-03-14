1864a1dc95e30a90c423946917b0057e5cbbc536
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2026-03-14 16:44:47 -0400

    Time: 847 ms (5.42%), Space: 19.6 MB (63.62%) - LeetHub

---
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        if n==1:
+            res = 'abc'
+            return res[k-1] if k<=3 else ''
+        possible = itertools.product('abc', repeat=n)
+        happies = []
+
+        def ishappy(tup):
+            for i in range(n-1):
+                if tup[i]==tup[i+1]:
+                    return False
+            return True
+
+        for tup in possible:
+            if ishappy(tup):
+                happies.append(tup)
+        
+        print(len(happies))
+        return ''.join(happies[k-1]) if len(happies)>=k else  '' 
+              
