2c11bfbe719729c867853437090f43f7c4bc963d
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2026-03-18 13:13:16 -0400

    Time: 133 ms (93.79%), Space: 55.7 MB (93.1%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
@@ -1,21 +1,34 @@
 class Solution:
     def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
-        n = len(grid)
-        m = len(grid[0])
-        res=0
+        n, m = len(grid), len(grid[0])
+        res = 0
+
+        col_sum = [0] * m
+
         for i in range(n):
+            row_sum = 0
             for j in range(m):
-                if i > 0:
-                    grid[i][j] += grid[i-1][j]
-                if j > 0:
-                    grid[i][j] += grid[i][j-1]
-                if i > 0 and j > 0:
-                    grid[i][j] -= grid[i-1][j-1]
-                    
-                if grid[i][j] <= k:
+                col_sum[j] += grid[i][j]
+                row_sum += col_sum[j]
+                
+                if row_sum <= k:
                     res += 1
 
         return res
+
+        # for i in range(n):
+        #     for j in range(m):
+        #         if i > 0:
+        #             grid[i][j] += grid[i-1][j]
+        #         if j > 0:
+        #             grid[i][j] += grid[i][j-1]
+        #         if i > 0 and j > 0:
+        #             grid[i][j] -= grid[i-1][j-1]
+
+        #         if grid[i][j] <= k:
+        #             res += 1
+
+        # return res
         
         # res=[0]
         # def dfs(r, c, curr):
