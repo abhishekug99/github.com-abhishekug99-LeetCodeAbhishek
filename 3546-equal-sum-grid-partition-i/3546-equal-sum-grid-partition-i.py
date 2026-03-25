313336b05f099994455f641853e3bd900d7340ad
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2026-03-25 17:30:42 -0400

    Time: 120 ms (77.55%), Space: 46.8 MB (40.82%) - LeetHub

---
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
@@ -0,0 +1,50 @@
+class Solution:
+    def canPartitionGrid(self, grid: List[List[int]]) -> bool:
+        n, m = len(grid), len(grid[0])
+        
+        # Row sums
+        horizontal = [sum(row) for row in grid]
+        total = sum(horizontal)
+        
+        prefix = 0
+        for i in range(n - 1): 
+            prefix += horizontal[i]
+            if prefix == total - prefix:
+                return True
+        
+        vertical = [0] * m
+        for j in range(m):
+            for i in range(n):
+                vertical[j] += grid[i][j]
+        
+        total = sum(vertical)
+        prefix = 0
+        for j in range(m - 1):
+            prefix += vertical[j]
+            if prefix == total - prefix:
+                return True
+        
+        return False
+        # Time consuming but correct
+        # horizontal=[]
+        # n,m =len(grid), len(grid[0])
+        # hor=0
+        # vertical=[]
+        # for i in range(n):
+        #     horizontal.append(sum(grid[i]))
+
+        # for i in range(n):
+        #     if sum(horizontal[:i+1])==sum(horizontal[i+1:]):
+        #         return True
+        
+        # for i in range(m):
+        #     curr=0
+        #     for j in range(n):
+        #         curr+=grid[j][i]
+        #     vertical.append(curr)
+        
+        # for i in range(m):
+        #     if sum(vertical[:i+1])==sum(vertical[i+1:]):
+        #         return True
+
+        # return False
