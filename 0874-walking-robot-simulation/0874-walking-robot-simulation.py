aaa6d1d7b3745d3a9485d59ae64ee367c91e0b36
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2026-04-06 10:32:51 -0400

    Time: 39 ms (53.86%), Space: 24.3 MB (14.98%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.py
@@ -0,0 +1,27 @@
+class Solution:
+    def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
+        x=0
+        y=0
+        direct = [[0,1],[1,0],[0,-1],[-1,0]] #posible direction N,E,S,W
+
+        d = 0
+        res = 0
+        obstacles = {tuple(o) for o in obstacles} #set comprehension
+        print(obstacles)
+        for c in commands:
+            if c==-1:
+                d = (d+1)%4 #used mod for not to go outof bound from direction index
+            elif c==-2:
+                d=(d-1)%4
+            else: 
+                dx,dy = direct[d] # till here we were tracking the directions
+                for _ in range(c): 
+                    if (x+dx, y+dy) in obstacles:
+                        break
+                    x,y = x+dx, y+dy #gives the magnitude
+            
+            res = max(res,x**2+y**2)
+
+
+
+        return res
