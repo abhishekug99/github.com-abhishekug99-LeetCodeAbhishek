a8983dbf3f9ab58054d6d338a4d1675eef8abbca
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2026-04-15 11:32:40 -0400

    Time: 0 ms (100%), Space: 19.5 MB (20.97%) - LeetHub

---
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def closestTarget(self, words: List[str], target: str, startIndex: int) -> int:
+        if target not in words:
+            return -1
+        cnt1=0
+        cnt2=0
+        n = len(words)
+        i=startIndex
+        while True:
+            if words[i] == target:
+                break
+            
+            i = ((i + 1) % n)
+            cnt1+=1
+        i=startIndex
+        while True:
+            if words[i] == target:
+                break
+            
+            i = ((i - 1 + n) % n)
+            cnt2+=1
+        
+        return min(cnt1, cnt2)
