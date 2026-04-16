f060bb1cdc2b2f8c936602690a009a9945be8232
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2026-04-16 12:21:23 -0400

    Time: 361 ms (53.91%), Space: 54.8 MB (64.84%) - LeetHub

---
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.py
@@ -0,0 +1,62 @@
+class Solution:
+    def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        mapnums = defaultdict(list)
+        n = len(nums)
+
+        for i, v in enumerate(nums):
+            mapnums[v].append(i)
+
+        res = []
+
+        for q in queries:
+            curr = nums[q]
+            positions = mapnums[curr]
+
+            if len(positions) == 1:
+                res.append(-1)
+                continue
+
+            idx = bisect_left(positions, q)
+
+            left = positions[idx - 1]  # circular left
+            right = positions[(idx + 1) % len(positions)]  # circular right
+
+            d1 = abs(q - left)
+            d2 = abs(q - right)
+
+            dist = min(d1, n - d1, d2, n - d2)
+            res.append(dist)
+
+        return res
+            
+        
+        # Correct but TLE 612/614
+        # mapnums = defaultdict(list)
+        # res = []
+        # n=len(nums)
+        # if len(nums) == len(set(nums)):
+        #     return [-1]*len(queries)
+
+        # for i in range(len(nums)):
+        #     mapnums[nums[i]].append(i)
+
+        # # print(mapnums)
+        # for q in queries:
+        #     curr = nums[q]
+        #     # minDist = float('inf')
+        #     idxPos = mapnums[curr]
+        #     if len(idxPos)==1:
+        #         res.append(-1)
+        #         continue
+        #     for i in range(len(idxPos)):
+        #         if mapnums[curr][i] == q:
+        #             idx=i
+        #     l = mapnums[curr][idx-1]
+        #     r = mapnums[curr][(idx+1)%len(idxPos)]
+        #     d1 = abs(q-l)
+        #     d2 = abs(q-r)
+        #     dist = min(d1,n-d1,d2,n-d2)
+        #         # dist = (abs(mapnums[curr][idx]-mapnums[curr][idx-1]))%n
+        #         # minDist = min(minDist, dist)
+        #     res.append(dist)
+        # return res
