d1cd5be1331b9fbfcd8c9f649a66181c4c00a949
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2026-04-28 10:48:06 -0400

    Time: 209 ms (51.11%), Space: 35.5 MB (61.48%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
@@ -10,31 +10,49 @@ class Solution:
         5: [(0,-1),(-1,0)],
         6: [(0,1),(-1,0)],
         }
-        donePaths = set()
+        
         
         def canComeBack(nr,nc,r,c):
             for dr,dc in connectionMap[grid[nr][nc]]:
                 if nr+dr==r and nc+dc==c:
                     return True
             return False
-
-        def dfs(r,c):
-            if (r,c) in donePaths:
-                return False
-
-            donePaths.add((r,c))
-
-            if r == n-1 and c == m-1:
-                return True
-            
-            for dr,dc in connectionMap[grid[r][c]]:
-                nr,nc = dr+r, dc+c
-                if 0 <= nr < n and 0 <= nc < m:
-                    if canComeBack(nr,nc,r,c):
-                        if dfs(nr,nc):
-                            return True
-            
-            return False  
         
-        return dfs(0,0)
+        #bfs approach
+        visited = set()
+        queue = deque([(0,0)])
+        visited.add((0,0))
+        
+        while queue:
+            r,c = queue.popleft()
+            if r==n-1 and c==m-1:
+                return True
+            for dr,dc in connectionMap[grid[r][c]]:
+                nr,nc = r+dr, c+dc
+                if 0<=nr<n and 0<=nc<m:
+                    if (nr,nc) not in visited and canComeBack(nr,nc,r,c):
+                        visited.add((r,c))
+                        queue.append((nr,nc))
+        return False
+        
+        
+        # def dfs(r,c):
+        #     if (r,c) in donePaths:
+        #         return False
+
+        #     donePaths.add((r,c))
+
+        #     if r == n-1 and c == m-1:
+        #         return True
+            
+        #     for dr,dc in connectionMap[grid[r][c]]:
+        #         nr,nc = dr+r, dc+c
+        #         if 0 <= nr < n and 0 <= nc < m:
+        #             if canComeBack(nr,nc,r,c):
+        #                 if dfs(nr,nc):
+        #                     return True
+            
+        #     return False  
+        
+        # return dfs(0,0)
 
