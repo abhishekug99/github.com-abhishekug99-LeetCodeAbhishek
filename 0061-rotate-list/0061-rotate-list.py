06e5e7aaff28dee62e8703ececb44e3b6f930895
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2026-05-05 12:28:22 -0400

    Time: 0 ms (100%), Space: 19.4 MB (36.28%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -7,45 +7,43 @@ class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if not head or not head.next or k == 0:
             return head
-
-        def getLen(head):
-            cnt =0
-            if not head:
-                return None
-            while head:
-                head = head.next
-                cnt+=1
-            return cnt
         
-        length = getLen(head)
-        k = k % length
+        def getlenLL(head):
+            if not head:
+                return 0
+            return 1 + getlenLL(head.next)
+        
+        length = getlenLL(head)
         if k == 0:
             return head
-        cut = length - k
-
-        dummy1 = ListNode(0)
-        dummy2 = ListNode(0)
-        cur = head
-        curD1 = dummy1
-        curD2 = dummy2
-        i=0
-        while cur:
-            if i<cut:
-                curD1.next = cur
-                curD1 = curD1.next
-            else:
-                curD2.next = cur
-                curD2 = curD2.next
-            i+=1
-            cur = cur.next
-
-        curD1.next = None
-        curD2.next = dummy1.next
-
-        return dummy2.next
-
-
-
-
-
+        k = k % length
         
+        cut = length - k
+        
+        d1 = ListNode(0)
+        d2 = ListNode(0)
+
+        curr = head
+        c1 = d1
+        c2 = d2
+        i=0
+
+        while curr:
+            if i<cut:
+                c1.next = curr
+                c1 = c1.next
+            else:
+                c2.next = curr
+                c2 = c2.next
+            
+            i+=1
+            curr=curr.next
+        c1.next = None
+        c2.next  = d1.next
+
+        return d2.next
+
+
+
+
+
