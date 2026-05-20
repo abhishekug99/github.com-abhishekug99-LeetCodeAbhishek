6c3327b66e611bf7c958cd11b9ac004a0e7b8f51
Abhishek Gavali <60284276+abhishekug99@users.noreply.github.com>
2026-05-20 17:55:11 -0400

    Time: 10 ms (47.97%), Space: 19.2 MB (94.36%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.py
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.py
@@ -1,20 +1,36 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-        res = [0]*len(A)
         n = len(A)
-        adict = defaultdict(list)
-        bdict = defaultdict(list)
-
+        freq = {}
+        common = 0
+        res = []
         for i in range(n):
-            adict[i] = A[:i+1]
-        
-        for i in range(n):
-            bdict[i] = B[:i+1]
 
-        # print(adict)
-        for i in range(len(A)):
-            common = list(set(adict[i]) & set(bdict[i]))
-            res[i] = len(common)
-        
+            freq[A[i]] = freq.get(A[i], 0) + 1
+            if freq[A[i]] == 2:
+                common += 1
+
+            freq[B[i]] = freq.get(B[i], 0) + 1
+            if freq[B[i]] == 2:
+                common += 1
+            res.append(common)
         return res
+
+        #submitted but O(n^2)
+        # res = [0]*len(A)
+        # adict = defaultdict(list)
+        # bdict = defaultdict(list)
+
+        # for i in range(n):
+        #     adict[i] = A[:i+1]
+        
+        # for i in range(n):
+        #     bdict[i] = B[:i+1]
+
+        # # print(adict)
+        # for i in range(len(A)):
+        #     common = list(set(adict[i]) & set(bdict[i]))
+        #     res[i] = len(common)
+        
+        # return res
 
